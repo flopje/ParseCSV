3a670d650cbdbe3525087a7d40fc652601a27205
Floris <>
2018-02-22 21:32:39 +0100

    Added basics database support via dbFlow

--- app/src/main/java/com/flowapps/parseCSV/service/models/Person.kt
+++ app/src/main/java/com/flowapps/parseCSV/service/models/Person.kt
@@ -1,6 +1,24 @@
 package com.flowapps.parseCSV.service.models
 
+import com.flowapps.parseCSV.service.database.AppDatabase
+import com.raizlabs.android.dbflow.annotation.Column
+import com.raizlabs.android.dbflow.annotation.PrimaryKey
+import com.raizlabs.android.dbflow.annotation.Table
+import com.raizlabs.android.dbflow.kotlinextensions.from
+import com.raizlabs.android.dbflow.kotlinextensions.list
+import com.raizlabs.android.dbflow.kotlinextensions.select
 import java.util.*
 
+@Table(database = AppDatabase::class)
+class Person(@Column var firstName: String? = null,
+             @Column var lastName: String? = null,
+             @Column var issueCount: String? = null,
+             @Column var dateOfBirth: Date? = null) {
 
-class Person(val firstName: String, val lastName: String, val issueCount: String, val dateOfBirth: Date)
+    @PrimaryKey(autoincrement = true)
+    var id: Int = 0
+}
+
+fun selectAllPersona() : List<Person> {
+    return (select from Person::class).list
+}
