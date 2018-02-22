04949710a1c7963e3b8a8e82c7a21cf27b2be8c2
Floris <>
2018-02-22 21:32:39 +0100

    Added basics database support via dbFlow

--- app/src/main/java/com/flowapps/parseCSV/App.kt
+++ app/src/main/java/com/flowapps/parseCSV/App.kt
@@ -2,6 +2,7 @@ package com.flowapps.parseCSV
 
 import android.app.Application
 import android.content.Context
+import com.raizlabs.android.dbflow.config.FlowManager
 
 
 class App : Application() {
@@ -16,5 +17,10 @@ class App : Application() {
         fun applicationContext() : Context {
             return instance!!.applicationContext
         }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        FlowManager.init(this)
     }
 }
